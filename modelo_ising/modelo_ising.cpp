8dfab6092641fd10a8ea48fb1c77d60b24360fea
Eduavite123 <eduavilestejada@gmail.com>
2023-04-17 15:56:53 +0200

    launc.json fail

--- modelo_ising/modelo_ising.cpp
+++ modelo_ising/modelo_ising.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <cmath>
-//#include "gsl_rng.h"
+#include <stdlib.h>  //srand(), radn() (provisional hasta ver cómo funciona gsl)
+#include <time.h>
 using namespace std;
 
 #define MAX 100
-//gsl_rng *tau;
 
 int main(){
     //----------------------DECLARACIÓN DE VARIABLES-----------------------------------
-    //extern gsl_rng *tau; //hay que añadirlo en cada función
     int N; //dimensión de la red
     int s[MAX][MAX]; //espin de cada nodo de la red (1 o -1)
     //REPASAR DECLARACION DE MATRICES LIBRO BERNIER
@@ -16,6 +15,17 @@ int main(){
     double p; //probabilidad 
     double e; //número aleatorio para comparar con p
     int choose; //para elegir espines alineados o desalineados aleatoriamente
+    //---------------------------------------------------------------------------------
+
+    //----------------------VALORES INICIALES------------------------------------------
+    //El usuario elegirá los valores iniciales:
+    cout << "Elija los valores iniciales de los espines (0:todos alineados, 1:desalineados aleatoriamente):" << endl;
+    cin >> choose;
+    while(choose!=0 || choose!=1)
+    {
+        cout << "Por favor, elija una opción adecuada (0:todos alineados, 1:desalineados aleatoriamente):" << endl;
+        cin >> choose;
+    }
     //---------------------------------------------------------------------------------
 
     return 0;
