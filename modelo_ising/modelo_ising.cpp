b71c1390d8c11a01daf6e5f80607b5ce156cc2c1
Eduavite123 <eduavilestejada@gmail.com>
2023-04-17 15:22:22 +0200

    comienzo ising

---
+++ modelo_ising/modelo_ising.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+//#include "gsl_rng.h"
+using namespace std;
+
+#define MAX 100
+//gsl_rng *tau;
+
+int main(){
+    //----------------------DECLARACIÓN DE VARIABLES-----------------------------------
+    //extern gsl_rng *tau; //hay que añadirlo en cada función
+    int N; //dimensión de la red
+    int s[MAX][MAX]; //espin de cada nodo de la red (1 o -1)
+    //REPASAR DECLARACION DE MATRICES LIBRO BERNIER
+    double T; //temperatura (entre 0 y 5)
+    double p; //probabilidad 
+    double e; //número aleatorio para comparar con p
+    int choose; //para elegir espines alineados o desalineados aleatoriamente
+    //---------------------------------------------------------------------------------
+
+    return 0;
+}
