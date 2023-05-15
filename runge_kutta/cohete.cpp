4c786dfa5d343e3a14ba3a74ac8ad58c2e2701ac
Eduavite123 <eduavilestejada@gmail.com>
2023-05-15 18:06:10 +0200

    terminado???

--- runge_kutta/cohete.cpp
+++ runge_kutta/cohete.cpp
@@ -29,6 +29,8 @@ int main(){
     double k1[4], k2[4], k3[4], k4[4];
     double r_prima;
     double aux[4];
+    double x,y; //Posición del cohete en coordenadas cartesianas
+    double x_L,y_L; //Posición de la Luna en coordenadas cartesianas
     //-------------------------------------------------------------------------------------
     
     //--------------------------CONDICIONES INICIALES--------------------------------------
@@ -43,6 +45,10 @@ int main(){
     y[2]=11200.0/dTL; //Componente radial velocidad inicial = velocidad de escape terrestre = 11.2 km/s
     y[3]=0; //Componente angular velocidad inicial nula => Sale normal a la superficie terrestre
     t=0;
+
+    //Condiciones iniciales de la Luna
+    x_L=1.0*cos(0.0);
+    y_L=1.0*sin(0.0);
     //-------------------------------------------------------------------------------------
 
     //-------------------------------RUNGE_KUTTA-------------------------------------------
@@ -50,6 +56,16 @@ int main(){
     pos.open("cohete_data.dat");
     for (int i = 0; i < iter; i++)
     {
+        //Pasamos de coordenadas cilíndricas a cartesianas
+        x=y[0]*cos(y[1]);
+        y=y[0]*sin(y[1]); 
+
+        //Escribimos los datos en el archivo
+        pos << 0.0 << ", " << 0.0 << endl; //Posición Tierra (fija)
+        pos << x_L << ", " << y_L << endl; //Posición Luna
+        pos << x << ", " << y << endl;
+        pos << endl;
+
         //Evaluamos k1
         k1[0]=h*f0(y[2]);
         k1[1]=h*f1(y[0],y[3]);
@@ -96,7 +112,11 @@ int main(){
             y[k]=y[k]+1.0/6.0*(k1[k]+2*k2[k]+2*k3[k]+k4[k]);
         }
 
-        t=t+h;          
+        t=t+h; 
+
+        //Movemos la Luna
+        x_L=1.0*cos(w*t);
+        y_L=1.0*sin(w*t);         
 
     }
     pos.close();
